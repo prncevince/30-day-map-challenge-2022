6c0e067f4cea1c3831dfaaa3b1a58b558e8a08b0
Vincent Clemson <vclemson07@gmail.com>
2022-11-03 10:33:47 -0400

    initialize bare renv

---
+++ .Rprofile
@@ -0,0 +1,3 @@
+# Suppress warnings when !expr values are in R Markdown yaml header
+options(yaml.eval.expr = TRUE)
+source("renv/activate.R")
---
+++ deps.R
@@ -0,0 +1,14 @@
+# analysis
+library(data.table)
+library(ggplot2)
+library(plotly)
+library(sf)
+library(tictoc)
+# R Markdown
+library(rmarkdown)
+# R CLI nvim coc / ALE setup
+library(languageserver)
+library(styler)
+library(lintr)
+# RStudio
+library(rstudioapi)
